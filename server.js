ccda74bc8c74497209f94e98906c0f4af012fc90
mibrahim234 <mohamm1417@gmail.com>
2022-05-06 05:49:25 -0400

    post route draft

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const app = express();
 // handles json data (middleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+app.post('/test', (req, res) => {
+    const string_to_cut = 
+    res.json({return_string: string_to_cut})
+})
+
+app.listen(3000, () => {
+    console.log('On Port 3000!')
+    })
