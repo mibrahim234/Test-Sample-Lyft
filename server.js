f88240fe2f982bc4f5441176d0dfd240523fa452
mibrahim234 <mohamm1417@gmail.com>
2022-05-06 05:48:27 -0400

    middleware

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const express = require ('express')
+// initialize express server 
 const app = express();
+
+// handles json data (middleware)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
